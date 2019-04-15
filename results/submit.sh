fb7539b3c9c955c5d3cac864d7b90f09175570b9
chandan.maruthi@gmail.com <chandan.maruthi@gmail.com>
2019-04-15 15:05:38 -0700

    latest

--- results/submit.sh
+++ results/submit.sh
@@ -1,2 +1,2 @@
- kaggle competitions submit -c kaggledays-sf-hackathon -f submission.csv -m "totally silly"
+kaggle competitions submit -c kaggledays-sf-hackathon -f submit_results.csv -m "totally silly"
 
